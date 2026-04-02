3f06bc1a856b228ad6efa29e17d4ad30c6e1ce8c
Erick Sebastián Velásquez Vera <erick.velasquez@udla.edu.ec>
2026-04-02 15:26:42 -0500

    vector.c

--- vector.c
+++ vector.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main ()
+int main () {
+int n;
 {
-    int A[3];
-
-    A[0]=25;
-    A[1]=36;
-    A[2]=52;
-
-    for (int i=0; i < 3; i++) {
-        printf("%d\n", A[i]);
+    do {
+       printf ("Indique el tamaño del vector: ");
+    scanf ("%d", &n); 
     }
-    return 0;
+    if (n==0){
+        printf ("El tamaño no puede ser 0. Intente nuevamente. \n");
+    }
+} while (n==0);
+int A[n];
+srand (time(NULL));
+for (int i=0; i<n;i++){
+    A[i]=rand()%100+1;
+}
+for (int i=0; i<n;i++){
+    printf("%d ", A[i]);
+}
+return 0
 }
